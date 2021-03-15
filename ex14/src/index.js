b9be84ec2820d0a180604db0d50bc98c784e200c
basslines <admir.osmanovic@gmail.com>
2021-03-16 00:56:58 +0100

    dodao index.js fajl

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+function convertStrToInt(str) {
+  var num = parseInt(str);
+  return num;
+}
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+
+module.exports = convertStrToInt;
