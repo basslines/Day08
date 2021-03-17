648d7d8be07950a314bbd8702bd3fafacbe68b1d
basslines <admir.osmanovic@gmail.com>
2021-03-18 00:54:50 +0100

    dodao index.js fajl

---
+++ ex15/src/index.js
@@ -0,0 +1,10 @@
+function convertToInt(str) {
+  return parseInt(str, 2);
+}
+convertToInt("!011");
+console.log(a);
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+
+module.exports = convertToInt;
