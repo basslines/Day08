09fbc2dc78baccecd55f631139622da93755a279
basslines <admir.osmanovic@gmail.com>
2021-03-18 00:49:21 +0100

    dodao index.js fajl

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,14 +1,13 @@
 function localScope() {
   var myVariable = "I am here!";
   console.log(myVariable);
-
-  if (typeof myVariable != "undefined") {
-    console.log("outside localScope", myVariable);
-  } else {
-    console.log("outside localScope UNDEFINED!!!");
-  }
 }
 
-localScope();
+console.log(localScope());
+if (typeof myVariable != "undefined") {
+  console.log("outside localScope", myVariable);
+} else {
+  console.log("outside localScope UNDEFINED!!!");
+}
 
 module.exports = localScope;
